30c40b499c6b60545dfdf86ebf23d4472e35af9d
da-and <electrotec10@yahoo.com.ar>
2019-05-04 11:53:20 -0500

    practica 4 login y list view

---
+++ app/src/main/java/com/example/autonoma/practica4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.autonoma.practica4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
